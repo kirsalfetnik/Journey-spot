0294add2cc1e0f4990de95b56f10fecad5c3bfe1
Kirill Salfetnik <oriole001@gmail.com>
2023-05-27 15:28:55 +0200

    Implement register modal

--- frontend/app/components/Button.tsx
+++ frontend/app/components/Button.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { IconType } from "react-icons"
 
 interface ButtonProps {
@@ -15,10 +17,12 @@ const Button: React.FC<ButtonProps> = ({
     disabled,
     outline,
     small,
-    icon
+    icon: Icon
 }) => {
     return (
         <button
+            onClick={onClick}
+            disabled={disabled}
             className={`
                 relative
                 disabled:opacity-70
@@ -36,6 +40,16 @@ const Button: React.FC<ButtonProps> = ({
                 ${small ? 'border-[1px]' : 'border-2'}
             `}
         >
+            {Icon && (
+                <Icon 
+                    size={24}
+                    className="
+                        absolute
+                        left-4
+                        top-3
+                    "
+                />
+            )}
             {label}
         </button>
     );
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from 'next/font/google'
 
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={nunito.className}>
         <ClientOnly>
-          <Modal isOpen />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
