c2d964cdfc1673190677ddb3b85c599f0a48679a
Kirill Salfetnik <oriole001@gmail.com>
2023-05-29 15:37:27 +0200

    Implement reservation functionality

--- frontend/app/listings/[listingId]/ListingClient.tsx
+++ frontend/app/listings/[listingId]/ListingClient.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation"
 import { useCallback, useEffect, useMemo, useState } from "react"
 import { Reservation } from "@prisma/client"
 
-import { SafeListing, SafeUser } from "@/app/types"
+import { SafeListing, SafeUser, safeReservation } from "@/app/types"
 import { categories } from "@/app/components/navbar/Categories"
 import Container from "@/app/components/Container"
 import ListingHead from "@/app/components/listings/ListingHead"
@@ -24,7 +24,7 @@ const initialDateRange = {
 }
 
 interface ListingClientProps {
-    reservations?: Reservation[];
+    reservations?: safeReservation[];
     listing: SafeListing & {
         user: SafeUser
     };
